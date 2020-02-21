1db5cb9c766125e13dc48f88bcc7b4d1651cb4de
粟屋　拓馬 <awayatakuma@awayatakumanoMacBook-Pro.local>
2020-02-21 23:30:10 +0900

    bb

--- aoj_alds1_10_C_2.cpp
+++ aoj_alds1_10_C_2.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 typedef long long ll;
 typedef long double lb;
-#define MOD 1000000007
-#define REP(I,J,N) for (int I=J;I<(N);I++)
-#define RREP(i,n) for (int i = n; 0<i;i--)
+#define mod 1000000007
+#define rep(i,j,n) for (int i=j;i<(n);i++)
+#define rrep(i,n) for (int i = n; 0<i;i--)
 #define print(out) cout<< out  << "\n";
 
 int main(){
